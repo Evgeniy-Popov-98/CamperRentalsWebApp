9c971778a3d400b9be771dde30ba1b97c3ab8e57
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-08-10 11:45:41 +0300

    firstTry_addPagination

--- src/components/CatalogList/CatalogList.jsx
+++ src/components/CatalogList/CatalogList.jsx
@@ -1,22 +1,33 @@
 import { useSelector } from 'react-redux';
 import { CatalogItem } from '../CatalogItem/CatalogItem';
 import { selectAllCampers } from '../../redux/camper/selectors';
+import { pagination } from '../../utils/pagination';
 
 import css from './CatalogList.module.css';
+import { useState } from 'react';
 
 export const CatalogList = () => {
-  const campers = useSelector(selectAllCampers);
+  const campers = pagination(useSelector(selectAllCampers));
+  const [index, setIndex] = useState(0);
 
   return (
     <div>
       <ul className={css.canperList}>
         {Array.isArray(campers) &&
-          campers.map(camper => (
+          campers[index].map(camper => (
             <li key={camper._id}>
               <CatalogItem item={camper} />
             </li>
           ))}
       </ul>
+      <button
+        type="submit"
+        onClick={() => {
+          setIndex(index + 1);
+        }}
+      >
+        Load more
+      </button>
     </div>
   );
 };
