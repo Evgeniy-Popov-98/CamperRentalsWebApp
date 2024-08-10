329decd3c23441b92fea9a472be814c315bbd213
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-08-10 12:34:46 +0300

    test

--- src/components/CatalogList/CatalogList.jsx
+++ src/components/CatalogList/CatalogList.jsx
@@ -4,28 +4,42 @@ import { selectAllCampers } from '../../redux/camper/selectors';
 import { pagination } from '../../utils/pagination';
 
 import css from './CatalogList.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const CatalogList = () => {
-  const campers = pagination(useSelector(selectAllCampers));
+  const campers = useSelector(selectAllCampers);
   const [index, setIndex] = useState(0);
+  const [campersArr, setCampersArr] = useState([]);
 
+  useEffect(() => {
+    async function dataCamper() {
+      try {
+        const data = await pagination(campers);
+        console.log(data);
+
+        setCampersArr(data);
+      } catch (error) {}
+    }
+    dataCamper();
+  });
+
+  const upLoadList = () => {
+    setIndex(index + 1);
+    setCampersArr(prevState => [...prevState, ...campers[index]]);
+  };
+
+  console.log(campersArr);
   return (
     <div>
       <ul className={css.canperList}>
-        {Array.isArray(campers) &&
-          campers[index].map(camper => (
+        {Array.isArray(campersArr) &&
+          campersArr.map(camper => (
             <li key={camper._id}>
               <CatalogItem item={camper} />
             </li>
           ))}
       </ul>
-      <button
-        type="submit"
-        onClick={() => {
-          setIndex(index + 1);
-        }}
-      >
+      <button type="submit" onClick={() => upLoadList()}>
         Load more
       </button>
     </div>
