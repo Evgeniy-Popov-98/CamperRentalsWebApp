1c57583a20ab00ef918c6fe67b5e74952ba91869
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-08-10 20:21:20 +0300

    add_pagina

--- src/components/CatalogList/CatalogList.jsx
+++ src/components/CatalogList/CatalogList.jsx
@@ -1,20 +1,13 @@
-import { useSelector } from 'react-redux';
 import { CatalogItem } from '../CatalogItem/CatalogItem';
-import { selectAllCampers } from '../../redux/camper/selectors';
-import { pagination } from '../../utils/pagination';
 
 import css from './CatalogList.module.css';
-import { useEffect, useState } from 'react';
 
-export const CatalogList = () => {
-  const campers = pagination(useSelector(selectAllCampers));
-  const [index, setIndex] = useState(0);
-
+export const CatalogList = ({ newArr, upDataArr }) => {
   return (
     <div className={css.campersListBox}>
       <ul className={css.campersList}>
-        {Array.isArray(campers) &&
-          campers[index].map(camper => (
+        {Array.isArray(newArr) &&
+          newArr.map(camper => (
             <li key={camper._id}>
               <CatalogItem item={camper} />
             </li>
@@ -24,8 +17,7 @@ export const CatalogList = () => {
         className={css.buttonLoadMore}
         type="submit"
         onClick={() => {
-          setIndex(index + 1);
-          //   setNewArr(campers[index]);
+          upDataArr();
         }}
       >
         Load more
--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,7 +1,11 @@
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { selectAllCampers } from '../../redux/camper/selectors';
+import { pagination, paginationNewArr } from '../../utils/pagination';
+
 import { FiltersBox } from '../../components/FiltersBox/FiltersBox';
 import { CatalogList } from '../../components/CatalogList/CatalogList';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
 import { fetchCampers } from '../../redux/camper/operations';
 
 import css from './CatalogPage.module.css';
@@ -9,14 +13,27 @@ import css from './CatalogPage.module.css';
 export const CatalogPage = () => {
   const dispatch = useDispatch();
 
+  const campers = pagination(useSelector(selectAllCampers));
+  const [index, setIndex] = useState(0);
+  const [newArr, setNewArr] = useState([]);
+
   useEffect(() => {
     dispatch(fetchCampers());
   }, [dispatch]);
 
+  useEffect(() => {
+    const data = paginationNewArr(campers, index);
+    setNewArr(prevState => [...prevState, ...data]);
+  }, [index]);
+
+  const upDataArr = () => {
+    if (index < campers.length - 1) setIndex(index + 1);
+  };
+
   return (
     <section className={css.catalogSection}>
       <FiltersBox />
-      <CatalogList />
+      <CatalogList newArr={newArr} upDataArr={upDataArr} />
     </section>
   );
 };
