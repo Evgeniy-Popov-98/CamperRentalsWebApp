e78e00dd32da5d92f099fa840e9048c77f63e1da
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-08-10 23:13:33 +0300

    fix_scrole

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -19,46 +19,48 @@ export const ModalWindow = ({ modalIsOpen, closeModal, item }) => {
       ariaHideApp={false}
     >
       <div className={css.modalBox}>
-        <h2 className={css.modalTitle}>{item.name}</h2>
-        <div className={css.modalSubTitleBox}>
-          <p className={css.SubTitleRating}>{item.rating}</p>
-          <p className={css.SubTitleLocation}> {item.location}</p>
-        </div>
-        <p className={css.modalPrice}>&#x20ac;{item.price}</p>
-        <div className={css.modalImgBox}>
-          {Array.isArray(item.gallery) &&
-            item.gallery.map((img, index) => (
-              <img key={index} className={css.modalImg} src={img} alt="" />
-            ))}
-        </div>
-        <p className={css.modalDescription}>{item.description}</p>
-        <div className={css.detailsBtnBox}>
-          <button
-            className={css.detailsBtn}
-            onClick={() => {
-              setIsActiv(true);
-            }}
-          >
-            Features
-          </button>
-          <button
-            className={css.detailsBtn}
-            onClick={() => {
-              setIsActiv(false);
-            }}
-          >
-            Reviews
-          </button>
-        </div>
-        <hr className={css.detailsLine} />
-        <div className={css.detailsBottom}>
-          {isActiv ? (
-            <FeaturesInfo item={item} />
-          ) : (
-            <ReviewsInfo reviews={item.reviews} />
-          )}
+        <div style={{ width: '902px' }}>
+          <h2 className={css.modalTitle}>{item.name}</h2>
+          <div className={css.modalSubTitleBox}>
+            <p className={css.SubTitleRating}>{item.rating}</p>
+            <p className={css.SubTitleLocation}> {item.location}</p>
+          </div>
+          <p className={css.modalPrice}>&#x20ac;{item.price}</p>
+          <div className={css.modalImgBox}>
+            {Array.isArray(item.gallery) &&
+              item.gallery.map((img, index) => (
+                <img key={index} className={css.modalImg} src={img} alt="" />
+              ))}
+          </div>
+          <p className={css.modalDescription}>{item.description}</p>
+          <div className={css.detailsBtnBox}>
+            <button
+              className={css.detailsBtn}
+              onClick={() => {
+                setIsActiv(true);
+              }}
+            >
+              Features
+            </button>
+            <button
+              className={css.detailsBtn}
+              onClick={() => {
+                setIsActiv(false);
+              }}
+            >
+              Reviews
+            </button>
+          </div>
+          <hr className={css.detailsLine} />
+          <div className={css.detailsBottom}>
+            {isActiv ? (
+              <FeaturesInfo item={item} />
+            ) : (
+              <ReviewsInfo reviews={item.reviews} />
+            )}
 
-          <ModalForm />
+            <ModalForm />
+          </div>
         </div>
       </div>
     </ModalReact>
