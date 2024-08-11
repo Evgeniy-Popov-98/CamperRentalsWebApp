db6e2912e8adb0806e7676fd958c8f6fe533067a
Yevhenii <143257644+Evgeniy-Popov-98@users.noreply.github.com>
2024-08-11 17:16:40 +0300

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { camperReducer } from './camper/slice';
 import { favoritesReducer } from './favorites/slice';
+import { filtersReducer } from './filters/slice';
 
 export const store = configureStore({
-  reducer: { campers: camperReducer, favorites: favoritesReducer },
+  reducer: {
+    campers: camperReducer,
+    favorites: favoritesReducer,
+    filters: filtersReducer,
+  },
 });
